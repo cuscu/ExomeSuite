2fd5855addae10cc4ea67484d7c627ee79400fb7
Pascual Lorente Arencibia <agua.cuscu@gmail.com>
2014-06-09 08:19:47 +0100

    Initialize

---
+++ src/exomesuite/MainViewController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exomesuite;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author uichuimi03
+ */
+public class MainViewController {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+
+    public void initialize() {
+    }
+
+}
